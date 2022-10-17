c9bc8e569420d02e8d3e7cb8999811f3e6dd7641
Marcel Wiessler <marcel@gaisterhand.de>
2022-10-17 11:10:16 +0200

    init

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,13 @@
 # abort on errors
 set -e
 
+cd docs
+
 # build
-npm run docs:build
+npm run build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd src/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -20,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:marwie/wiki-test.git master:gh-pages
+# git push -f git@github.com:marwie/wiki-test.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++
@@ -1,3 +0,0 @@
-{
-    base: "/wiki-test/"
-}
