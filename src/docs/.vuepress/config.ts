11347e799f3751dcb3e3d554ee771a7650d9c3d5
Marcel Wiessler <marcel@gaisterhand.de>
2022-10-22 11:06:07 +0200

    repro for vuepress build error

--- src/docs/.vuepress/config.ts
+++ src/docs/.vuepress/config.ts
@@ -1,9 +1,9 @@
 import { defineUserConfig, defaultTheme } from 'vuepress'
-import { searchPlugin } from '@vuepress/plugin-search'
-// import { mermaidPlugin } from "@renovamen/vuepress-plugin-mermaid";
-import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
-import { getDirname, path } from '@vuepress/utils'
-import * as videoplayer from "vuepress-plugin-core-video-player";
+// import { searchPlugin } from '@vuepress/plugin-search'
+// // import { mermaidPlugin } from "@renovamen/vuepress-plugin-mermaid";
+// import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
+// import { getDirname, path } from '@vuepress/utils'
+// import * as videoplayer from "vuepress-plugin-core-video-player";
 
 
 export default defineUserConfig({
@@ -11,55 +11,55 @@ export default defineUserConfig({
     lang: 'en-US',
     title: 'Needle Engine Documentation',
     description: 'Needle Engine is a web-based runtime for 3D apps. It runs on your machine for development, and can be deployed anywhere. It is flexible, extensible, and collaboration and XR come naturally.',
-    theme: defaultTheme({
-        logo: "/logo.png",
-        navbar: [
-            {
-                text: 'Home',
-                link: '/',
-            },
-            {
-                text: 'Scripting',
-                link: '/scripting',
-            },
-            {
-                text: 'Testing',
-                link: '/test',
-            },
-        ],
-        repo: "needle-tools/needle-engine-support",
-        docsRepo: "marwie/wiki-test",
-        docsBranch: "dev",
-        docsDir: "src/docs",
-        locales: {
-            '/': {
-                selectLanguageName: 'English',
-            },
-            // '/zh/': {
-            //   selectLanguageName: '简体中文',
-            // },
-        },
-    }),
-    locales: {
-        '/': {
-            lang: 'en-US',
-            title: 'Needle Engine Documentation',
-            description: 'Needle Engine is a web-based runtime for 3D apps. It runs on your machine for development, and can be deployed anywhere. It is flexible, extensible, and collaboration and XR come naturally.',
-        },
-        // '/zh/': {
-        //   lang: 'zh-CN',
-        //   title: 'VuePress',
-        //   description: 'Vue 驱动的静态网站生成器',
-        // },
-    },
-    plugins: [
-        // searchPlugin({
+    // theme: defaultTheme({
+    //     logo: "/logo.png",
+    //     navbar: [
+    //         {
+    //             text: 'Home',
+    //             link: '/',
+    //         },
+    //         {
+    //             text: 'Scripting',
+    //             link: '/scripting',
+    //         },
+    //         {
+    //             text: 'Testing',
+    //             link: '/test',
+    //         },
+    //     ],
+    //     repo: "needle-tools/needle-engine-support",
+    //     docsRepo: "marwie/wiki-test",
+    //     docsBranch: "dev",
+    //     docsDir: "src/docs",
+    //     locales: {
+    //         '/': {
+    //             selectLanguageName: 'English',
+    //         },
+    //         // '/zh/': {
+    //         //   selectLanguageName: '简体中文',
+    //         // },
+    //     },
+    // }),
+    // locales: {
+    //     '/': {
+    //         lang: 'en-US',
+    //         title: 'Needle Engine Documentation',
+    //         description: 'Needle Engine is a web-based runtime for 3D apps. It runs on your machine for development, and can be deployed anywhere. It is flexible, extensible, and collaboration and XR come naturally.',
+    //     },
+    //     // '/zh/': {
+    //     //   lang: 'zh-CN',
+    //     //   title: 'VuePress',
+    //     //   description: 'Vue 驱动的静态网站生成器',
+    //     // },
+    // },
+    // plugins: [
+    //     // searchPlugin({
 
-        // }),
-        // mermaidPlugin,
-        registerComponentsPlugin({
-            componentsDir: path.resolve(__dirname, './components'),
-        }),
-        // videoplayer
-    ],
+    //     // }),
+    //     // mermaidPlugin,
+    //     // registerComponentsPlugin({
+    //     //     componentsDir: path.resolve(__dirname, './components'),
+    //     // }),
+    //     // videoplayer
+    // ],
 })
