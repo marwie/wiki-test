5d4191076d6a750505ede3ebff11cb8e01babc67
Marcel Wiessler <marcel@gaisterhand.de>
2022-10-22 09:19:02 +0200

    github base

--- src/docs/.vuepress/config.ts
+++ src/docs/.vuepress/config.ts
@@ -1,10 +1,10 @@
 import { defineUserConfig, defaultTheme } from 'vuepress'
 
 export default defineUserConfig({
+    base: "/wiki-test/",
     lang: 'en-US',
     title: 'Needle Engine Documentation',
     description: 'Just playing around',
-    base: '/docs/',
     theme: defaultTheme({
         logo: "/logo.png",
         navbar: [
